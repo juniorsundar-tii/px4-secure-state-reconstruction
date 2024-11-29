ef33b3e217c58d6bc5cd7976d1b8eaf226cfe0ea
Junior Sundar <junior.sundar@tii.ae>
2024-11-29 15:13:50 +0400

    feat: Passthrough estimated states into the controller

--- px4_offboard_control/src/attacker.cpp
+++ px4_offboard_control/src/attacker.cpp
@@ -53,10 +53,10 @@ private:
 
 void Attacker::attack(VehicleLocalPosition &copy) {
     if (attack_flag) {
-        // copy.x *= 0.1*1;
+        copy.x *= 0.1*1;
         copy.vx *= 1;
         copy.y *= (1 + dist(generator));
-        copy.y *= 1;
+        /*copy.y *= 1;*/
         copy.vy *= 1;
     } else {
         copy.x *= 1;
